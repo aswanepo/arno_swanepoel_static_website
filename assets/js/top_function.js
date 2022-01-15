ea69e581830efb9c915d9caf413eea0a7b236135
arnonymous <arnonymous@kalidell.kalilan>
2022-01-15 18:32:37 +0200

    strip js and css

---
+++ assets/js/top_function.js
@@ -0,0 +1,21 @@
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//Get the button
+// var mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   console.log("hide button: ", document.body.scrollTop)
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//     console.log("show button")
+//   } else {
+//     mybutton.style.display = "none";
+//     console.log("hide button")
+//   }
+// }
