8c3f9d939874e961443fc01a8f36d237e531c253
arnonymous <arnonymous@kalidell.kalilan>
2022-01-18 07:38:53 +0200

    skybox

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,7 @@
       import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/mode=imports/optimized/three.js';
       //import * as GLTFLoader from 'https://cdn.rawgit.com/mrdoob/three.js/master/examples/js/loaders/GLTFLoader.js';
       //import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.js';
+      import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 
       const scene = new THREE.Scene();
       //const gltfLoader = new THREE.GLTFLoader();
@@ -91,6 +92,61 @@
 
       const spaceTexture = new THREE.TextureLoader().load('assets/images/space_4.jpg');
       scene.background = spaceTexture;
+
+
+
+
+
+
+
+
+        let controls = new OrbitControls( camera, renderer.domElement );
+
+        //let controls = new THREE.OrbitControls(camera);
+        controls.addEventListener('change', renderer);
+        controls.minDistance = 500;
+        controls.maxDistance = 1500;
+        
+        let materialArray = [];
+        let texture_ft = new THREE.TextureLoader().load( 'assets/images/skybox_front.png');
+        let texture_bk = new THREE.TextureLoader().load( 'assets/images/skybox_back.png');
+        let texture_up = new THREE.TextureLoader().load( 'assets/images/skybox_up.png');
+        let texture_dn = new THREE.TextureLoader().load( 'assets/images/skybox_down.png');
+        let texture_rt = new THREE.TextureLoader().load( 'assets/images/skybox_right.png');
+        let texture_lf = new THREE.TextureLoader().load( 'assets/images/skybox_left.png');
+          
+        materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+        materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+   
+        for (let i = 0; i < 6; i++)
+           materialArray[i].side = THREE.BackSide;
+        let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+        let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+        scene.add( skybox );  
+        //animate();
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       // Avatar
 
